60fe8c8e26062336257699517a582b82256ca322
ZarNizza <gormaster@mail.ru>
2022-08-10 20:15:45 +0400

    page title upd

--- my-project/myapp/app/Views/templates/header.php
+++ my-project/myapp/app/Views/templates/header.php
@@ -11,7 +11,7 @@
   <body>
    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #74b72e;">
                 <div class="container-fluid">
-                  <a class="navbar-brand" href="/">MVN Codeigniter tutorial</a>
+                  <a class="navbar-brand" href="/">Codeigniter<br/>tutorial</a>
                   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                   </button>
