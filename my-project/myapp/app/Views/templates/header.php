38b59373cc68982372cb9188d1f9adf80c03a515
ZarNizza <gormaster@mail.ru>
2022-08-10 18:58:03 +0400

    add assets/style

--- my-project/myapp/app/Views/templates/header.php
+++ my-project/myapp/app/Views/templates/header.php
@@ -5,6 +5,8 @@
     <title>MVN ci_1st_app</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://bootswatch.com/5/flatly/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php  echo base_url().':'.$_SERVER['SERVER_PORT'];?>/assets/css/style.css">
+
 </head>
   <body>
    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #74b72e;">
@@ -18,7 +20,7 @@
                     <ul class="navbar-nav me-auto">
                        <!--  href may need    echo base_url('url');  -->
                        <li class="nav-item">
-                         <a class="nav-link" href="/home">Process</a>
+                         <a class="nav-link" href="/news">News</a>
                        </li>
                       <li class="nav-item">
                         <a class="nav-link" href="/about">About</a>
